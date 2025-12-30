9c7974152f22311f6cc8f58835e371a1625d9f17
DiamondSoftS <oosoriojcristian@gmail.com>
2025-12-30 17:34:27 -0600

    Update IniciarSesion.js

--- src/IniciarSesion.js
+++ src/IniciarSesion.js
@@ -1,3 +1,6 @@
+import React from "react";
+import {useAuth0} from "@auth0/auth0-react";
+
 function IniciarSesion() {
   const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 
@@ -20,3 +23,4 @@ function IniciarSesion() {
 }
 
 export default IniciarSesion;
+
