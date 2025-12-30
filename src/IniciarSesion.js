867c6538ff7e88092faa700212a7633d5847bf12
DiamondSoftS <oosoriojcristian@gmail.com>
2025-12-30 17:29:57 -0600

    Update IniciarSesion.js

--- src/IniciarSesion.js
+++ src/IniciarSesion.js
@@ -1,45 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-/*const IniciarSesion = () => {
-  return (
-    <>
-      <center>
-        <br />
-        <br />
-        <h2>Login</h2>
-        <br />
-        <div className="form-group" style={{ width: 350, height: 200 }}>
-          <form>
-            <label>Username:</label>
-            <input type="text" className="form-control" />
-            <label>password:</label>
-            <br />
-            <input type="password" className="form-control" />
-            <br />
-            <button className="btn btn-outline-success form-control">
-              Login
-            </button>
-            <br />
-            <Link to="../Registrate">
-              <button className="btn btn-outline-warning form-control">
-                Registrarse
-              </button>
-            </Link>
-          </form>
-        </div>
-      </center>
-    </>
-  );
-};*/
-
-import { useAuth0 } from "@auth0/auth0-react";
-
 function IniciarSesion() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Iniciar sesión</button>;
+  return (
+    <div>
+      {!isAuthenticated ? (
+        <button onClick={() => loginWithRedirect()}>
+          Iniciar sesión
+        </button>
+      ) : (
+        <div>
+          <h2>Bienvenido {user.name}</h2>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Cerrar sesión
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default IniciarSesion;
-
