3a6169d74805ee618bdc268e20978e96ab1d16a9
DiamondSoftS <oosoriojcristian@gmail.com>
2026-05-12 07:18:04 -0600

    done

--- src/Inicio.js
+++ src/Inicio.js
@@ -190,7 +190,7 @@ publishable-key="pk_test_51QjATWGhdaW1xBvJQdafZMk5F0KYvEEXM3jgqDF9831JC9CSzVWHrx
             </p>
             <p> Soporte premium en español las 24 horas</p>
             <p>Programa de recompensas</p>
-            <h3 className="text-success">$3,700.00 </h3>
+            <h3 className="text-success">$4,900.00 </h3>
             <p>+4.7 comision por compra</p>
           </div>
         </div>
